ff10c85224ccbb0beab1688f05c33eead4711640
umhye1 <lucy021206@gmail.com>
2024-07-25 01:38:24 +0900

    구구단

---
+++ MultipleTable.py
@@ -0,0 +1,8 @@
+print("< 구구단 >")
+number = int(input("출력을 원하는 단을 입력 : "))
+
+# for i in range(9):
+#     print(number,"*",i+1,"=",number*(i+1))
+
+for i in range(1, 10):
+    print(number,"*",i,"=",number*i)
